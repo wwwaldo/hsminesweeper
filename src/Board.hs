dc7b1e4f74d0158e6a203b4986f82b81288820c5
Sönke Hahn <soenkehahn@gmail.com>
2018-06-12 16:23:27 -0400

    more stuff

--- src/Board.hs
+++ src/Board.hs
@@ -11,20 +11,10 @@ format (Board rows cols) =
 
       stuff :: [String]
       stuff = init $ concat $ replicate rows [cells, separator]
-  in
-  unlines ([horizontalborder] ++ stuff ++ [horizontalborder])
-
-
+  in unlines ([horizontalborder] ++ stuff ++ [horizontalborder])
 
 makeBoard :: Int -> Int -> Board
 makeBoard rows cols = Board rows cols
 
-myMap :: (a -> b) -> [a] -> [b]
-myMap f [] = []
-myMap f (x:xs) = f x : myMap f xs
-
-myReplicate :: Int -> a -> [a]
-myReplicate n a = case n of
-  0 -> []
-  n | n < 0 -> []
-  _ -> a : myReplicate (n - 1) a
+openCell :: Int -> Int -> Board -> Board
+openCell x y board = board
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -1,21 +1,24 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module BoardSpec where
 
 import Test.Hspec
 import Board
 
 spec = do
-	it "allows to open cells on a board" $ do
-		let board = makeBoard 2 2
-				newBoard = openCell 0 0 board
-				expected = unlines
-					[
-					"#####",
-					"|.| |",
-					"-----",
-					"| | |",
-					"#####"
-					]
-		format newBoard `shouldBe` expected
+  it "allows to open cells on a board" $ do
+    let board = makeBoard 2 2
+        newBoard = openCell 0 0 board
+        expected = unlines
+          [
+          "#####",
+          "|.| |",
+          "-----",
+          "| | |",
+          "#####"
+          ]
+    format newBoard `shouldBe` expected
 
 
   it "shows initial board" $ do
@@ -30,9 +33,6 @@ spec = do
           ]
     format board `shouldBe` expected
 
-
-    -- `` turns prefix functions into infix
-
   it "shows board of different size" $ do
     let board = makeBoard 1 1
         expected = unlines
@@ -41,5 +41,40 @@ spec = do
           "| |",
           "###"
           ]
-
     format board `shouldBe` expected
+
+  it "foo" $ do
+    putStrLn $ display True
+    putStrLn $ display (4 :: Int)
+    displayToStdOut (4 :: Int)
+    print (defaultValue :: Int)
+
+-- todo
+-- - typeclasses
+-- - IO
+
+class Display a where
+  display :: a -> String
+
+instance Show a => Display a where
+  display = show
+
+displayToStdOut :: Display a => a -> IO ()
+displayToStdOut = putStrLn . display
+
+x = 5
+
+f = \ x -> x + 2
+
+whiteList :: () -> [String]
+whiteList () = ["127.0.0.1", "localhost"]
+
+
+class Def a where
+  defaultValue :: a
+
+instance Def Bool where
+  defaultValue = False
+
+instance Def Int where
+  defaultValue = 42
