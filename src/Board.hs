443185db1354a061c940e8f4bd539663c65ffa09
Sönke Hahn <soenkehahn@gmail.com>
2018-06-15 18:36:30 -0400

    allow to open cells

--- src/Board.hs
+++ src/Board.hs
@@ -1,20 +1,40 @@
 module Board where
 
+import Data.Array.IArray
+import Data.List
+
 data Board =
-  Board Int Int
+  Board (Array (Int, Int) Bool)
+  deriving(Show)
 
 format :: Board -> String
-format (Board rows cols) =
-  let horizontalborder = replicate (2 * cols + 1) '#'
-      cells = concat (replicate cols "| ")  ++ "|"
+format board@(Board array) =
+  let
+      (rows, cols) = snd $ bounds array
+      horizontalborder = replicate (2 * cols + 1) '#'
+      cells = map (\ y -> formatRow y board  ++ "|") [1..rows]
+
       separator = replicate (2 * cols + 1) '-'
 
       stuff :: [String]
-      stuff = init $ concat $ replicate rows [cells, separator]
+      stuff = intersperse separator cells
   in unlines ([horizontalborder] ++ stuff ++ [horizontalborder])
 
+-- format the row belonging to board
+formatRow :: Int -> Board -> String
+formatRow row board@(Board array) = case row of
+  row -> concat $ map formatter [1 .. width board]
+  where
+    width :: Board -> Int
+    width (Board array) = snd $ snd $ bounds array
+
+    formatter :: Int -> String
+    formatter x = "|" ++ q
+      where q = if array!(row, x) == False then " " else "."
+
+-- index board coordinates from 1
 makeBoard :: Int -> Int -> Board
-makeBoard rows cols = Board rows cols
+makeBoard rows cols = Board $ listArray ((1, 1), (rows, cols)) (repeat False)
 
 openCell :: Int -> Int -> Board -> Board
-openCell x y board = board
+openCell row col board@(Board array) = Board $ array // [((row, col), True)]
--- test/BoardSpec.hs
+++ test/BoardSpec.hs
@@ -7,20 +7,6 @@ import Test.Hspec
 import Board
 
 spec = do
-  it "allows to open cells on a board" $ do
-    let board = makeBoard 2 2
-        newBoard = openCell 0 0 board
-        expected = unlines
-          [
-          "#####",
-          "|.| |",
-          "-----",
-          "| | |",
-          "#####"
-          ]
-    format newBoard `shouldBe` expected
-
-
   it "shows initial board" $ do
     let board = makeBoard 2 2
         expected = unlines
@@ -43,38 +29,15 @@ spec = do
           ]
     format board `shouldBe` expected
 
-  it "foo" $ do
-    putStrLn $ display True
-    putStrLn $ display (4 :: Int)
-    displayToStdOut (4 :: Int)
-    print (defaultValue :: Int)
-
--- todo
--- - typeclasses
--- - IO
-
-class Display a where
-  display :: a -> String
-
-instance Show a => Display a where
-  display = show
-
-displayToStdOut :: Display a => a -> IO ()
-displayToStdOut = putStrLn . display
-
-x = 5
-
-f = \ x -> x + 2
-
-whiteList :: () -> [String]
-whiteList () = ["127.0.0.1", "localhost"]
-
-
-class Def a where
-  defaultValue :: a
-
-instance Def Bool where
-  defaultValue = False
-
-instance Def Int where
-  defaultValue = 42
+  it "allows to open cells on a board" $ do
+    let board = makeBoard 2 2
+        newBoard = openCell 1 1 board
+        expected = unlines
+          [
+          "#####",
+          "|.| |",
+          "-----",
+          "| | |",
+          "#####"
+          ]
+    format newBoard `shouldBe` expected
